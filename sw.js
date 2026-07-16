b7e3aeb773ce1223cd1b49dbcbfd41323888c242
SenetUser <senetuser@example.com>
2026-07-16 16:01:48 +0500

    asd

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE = 'transfer-success-v5';
+const CACHE = 'transfer-success-v6';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './assets/check.svg', './assets/banner.png', './assets/cow.PNG', './assets/shoes.PNG'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
